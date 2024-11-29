506003cffce53526e3830112af8e69fa03d8f7fb
sunahara <atsushi_sunahara@token.co.jp>
2024-11-29 13:29:33 +0900

    シェアー処理追加

--- share.js
+++ share.js
@@ -5,13 +5,20 @@ const shareButton = document.getElementById('webShareButton');
 const descriptionMeta = document.querySelector("meta[name='description']");
 const description = descriptionMeta ? descriptionMeta.getAttribute("content") : '';
 
+// URLパラメータを取得
+const urlParams = new URLSearchParams(window.location.search);
+// パラメータを文字列として取得（例：param1=value1&param2=value2）
+const paramsString = urlParams.toString();
+
+const url = "https://asuna4993.github.io/ASuna4993/index.html" + (paramsString ? `?${paramsString}` : '');
+
 // クリックイベントを設定
 shareButton.addEventListener('click', async () => {
     try {
         await navigator.share({
             title: document.title,       // ページのタイトルを設定
             text: description,           // ページのdescriptionを設定
-            url: window.location.href    // 現在のURLを取得
+            url: url    // 現在のURLを取得
         });
         console.log('シェア成功！');
     } catch (error) {
